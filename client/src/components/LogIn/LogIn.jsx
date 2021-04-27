7b6135399aaa533aaa23ea286dff93d62804983a
Maggie Aytac <maggie.aytac@gmail.com>
2021-04-27 16:55:15 -0700

    fixed log in/out

--- client/src/components/LogIn/LogIn.jsx
+++ client/src/components/LogIn/LogIn.jsx
@@ -4,36 +4,21 @@ import LogOut from './LogOut.jsx';
 import Home from '../Home/Home.jsx';
 import styles from './styles.module.css';
 
-const LogIn = ({ fakeLogin }) => {
-  const isAuthenticated = true;
-  return (
-    <div className={styles.landingpage}>
-      {
-        !isAuthenticated && (
-          <div className={styles.maindiv}>
-            <h1 className={styles.heading}>
-              Local Voice
-            </h1>
-            <h5 className={styles.subheading}>
-              Bringing local issues to the palm of your hand
-            </h5>
-            <button type="button" onClick={() => fakeLogin()} className={styles.button}>
-              Log In
-            </button>
-          </div>
-        )
-      }
-      {
-        isAuthenticated && (
-          <div>
-            <LogOut />
-            <Home />
-          </div>
-        )
-      }
+const LogIn = ({ fakeLogin }) => (
+  <div className={styles.landingpage}>
+    <div className={styles.maindiv}>
+      <h1 className={styles.heading}>
+        Local Voice
+      </h1>
+      <h5 className={styles.subheading}>
+        Bringing local issues to the palm of your hand
+      </h5>
+      <button type="button" onClick={() => fakeLogin()} className={styles.button}>
+        Log In
+      </button>
     </div>
-  );
-};
+  </div>
+);
 
 LogIn.propTypes = {
   fakeLogin: PropTypes.func.isRequired,
