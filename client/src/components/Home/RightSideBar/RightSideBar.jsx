ce6e59224f2b7db8e26e2eac4427e9554bbec367
Maggie Aytac <maggie.aytac@gmail.com>
2021-04-27 13:55:09 -0700

    updated ad layout

--- client/src/components/Home/RightSideBar/RightSideBar.jsx
+++ client/src/components/Home/RightSideBar/RightSideBar.jsx
@@ -8,6 +8,13 @@ const RightSideBar = () => (
       <div className={styles.adBanner}>
         <div className={styles.adBannerVerticalLayout}>
           <div className={styles.adBannerPadding}>
+            <img className={styles.adImage} src="https://olbios.org/wp-content/uploads/2020/09/EllaBaker-1.jpg" alt="Open ad space" width="100%" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.adBanner}>
+        <div className={styles.adBannerVerticalLayout}>
+          <div className={styles.adBannerPadding}>
             <img className={styles.adImage} src="https://res.cloudinary.com/scalefunder/image/fetch/s--GjmGMm4n--/f_auto,fl_lossy,q_auto/https://res.cloudinary.com/scalefunder/image/upload/v1601580391/UC_Berkeley/q1gddukyaq0zjho8d9mp.png" alt="Open ad space" />
           </div>
         </div>
