5aae126b5f91e172fc59b11a389621d06442c0e6
Maggie Aytac <maggie.aytac@gmail.com>
2021-04-27 16:34:04 -0700

    remove more autho

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
+//import { Auth0Provider } from '@auth0/auth0-react';
 import App from './components/App.jsx';
 
 ReactDOM.render(
