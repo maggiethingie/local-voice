4645f6217dbe603c199be9af9345b26a858701a1
Maggie Aytac <maggie.aytac@gmail.com>
2021-04-27 16:27:47 -0700

    remove autho

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,12 +4,12 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import App from './components/App.jsx';
 
 ReactDOM.render(
-  <Auth0Provider
-    domain="dev-t4wdjpaw.us.auth0.com"
-    clientId="GAk2feXEjayezDcF1TyHggjnRG6DmdZS"
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
+  // <Auth0Provider
+  //   domain="dev-t4wdjpaw.us.auth0.com"
+  //   clientId="GAk2feXEjayezDcF1TyHggjnRG6DmdZS"
+  //   redirectUri={window.location.origin}
+  // >
+  <App />,
+  // </Auth0Provider>,
   document.getElementById('root'),
 );
