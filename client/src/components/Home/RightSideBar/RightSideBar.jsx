03de72082315c01d2ea1cded42ebc288202d31ee
Maggie Aytac <maggie.aytac@gmail.com>
2021-04-27 13:19:47 -0700

    improved ads

--- client/src/components/Home/RightSideBar/RightSideBar.jsx
+++ client/src/components/Home/RightSideBar/RightSideBar.jsx
@@ -8,14 +8,21 @@ const RightSideBar = () => (
       <div className={styles.adBanner}>
         <div className={styles.adBannerVerticalLayout}>
           <div className={styles.adBannerPadding}>
-            <img className={styles.adImage} src="https://i.ibb.co/sqBr23c/HR-Ad.png" alt="Open ad space" />
+            <img className={styles.adImage} src="https://res.cloudinary.com/scalefunder/image/fetch/s--GjmGMm4n--/f_auto,fl_lossy,q_auto/https://res.cloudinary.com/scalefunder/image/upload/v1601580391/UC_Berkeley/q1gddukyaq0zjho8d9mp.png" alt="Open ad space" />
           </div>
         </div>
       </div>
       <div className={styles.adBanner}>
         <div className={styles.adBannerVerticalLayout}>
           <div className={styles.adBannerPadding}>
-            <img className={styles.adImage} src="https://www.gofundme.com/c/wp-content/uploads/2021/03/SQUARE-1.png" alt="Open ad space" width="100%" />
+            <img className={styles.adImage} src="https://blog.toryburch.com/wp-content/uploads/2021/03/Culture_StopAsianHate_960_TD_Mar_21.jpg" alt="Open ad space" width="100%" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.adBanner}>
+        <div className={styles.adBannerVerticalLayout}>
+          <div className={styles.adBannerPadding}>
+            <img className={styles.adImage} src="https://olbios.org/wp-content/uploads/2020/09/EllaBaker-1.jpg" alt="Open ad space" width="100%" />
           </div>
         </div>
       </div>
